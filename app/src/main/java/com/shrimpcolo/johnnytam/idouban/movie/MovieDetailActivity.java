45ae454ca24f52f6ef5cdb0e6e93e453a1f3576f
JohnnyTam <shrimpcolo@gmail.com>
2016-07-22 10:04:37 +0800

    实现RecyclerView展示电影内容， 自定义Adapter， ViewHolder
           modified:   app/build.gradle
           modified:   app/src/main/AndroidManifest.xml
           new file:   app/src/main/java/com/shrimpcolo/johnnytam/idouban/movie/MovieDetailActivity.java
           modified:   app/src/main/java/com/shrimpcolo/johnnytam/idouban/movie/MoviesFragment.java
           new file:   app/src/main/res/layout/activity_movie_detail.xml
           new file:   app/src/main/res/layout/recyclerview_movies_item.xml
           modified:   app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/shrimpcolo/johnnytam/idouban/movie/MovieDetailActivity.java
@@ -0,0 +1,15 @@
+package com.shrimpcolo.johnnytam.idouban.movie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shrimpcolo.johnnytam.idouban.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
--- app/src/main/java/com/shrimpcolo/johnnytam/idouban/movie/MoviesFragment.java
+++ app/src/main/java/com/shrimpcolo/johnnytam/idouban/movie/MoviesFragment.java
@@ -1,18 +1,32 @@
 package com.shrimpcolo.johnnytam.idouban.movie;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.shrimpcolo.johnnytam.idouban.HomeActivity;
 import com.shrimpcolo.johnnytam.idouban.R;
 import com.shrimpcolo.johnnytam.idouban.api.DoubanManager;
 import com.shrimpcolo.johnnytam.idouban.api.IDoubanService;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +40,8 @@ import retrofit2.Response;
  */
 public class MoviesFragment extends Fragment {
     private List<Movies> mMoviesList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mMovieAdapter;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -45,10 +61,7 @@ public class MoviesFragment extends Fragment {
 
                 //debug
                 Log.e(HomeActivity.TAG, "===> Response, size = " + mMoviesList.size());
-                for (Movies movies : mMoviesList) {
-                    Log.e(HomeActivity.TAG, "title = " + movies.getTitle());
-                }
-                //end
+                mMovieAdapter.setData(mMoviesList);
             }
 
             @Override
@@ -64,12 +77,133 @@ public class MoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View view = inflater.inflate(R.layout.fragment_movies, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_hot_movies);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (mRecyclerView != null) {
+            mRecyclerView.setHasFixedSize(true);
+            final GridLayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(), 3);
+            mRecyclerView.setLayoutManager(layoutManager);
+
+            mMovieAdapter = new MoviesAdapter(getContext(), mMoviesList, R.layout.recyclerview_movies_item);
+
+            mRecyclerView.setAdapter(mMovieAdapter);
+        }
     }
 
     private void loadMovies(Callback<HotMoviesInfo> callback) {
         IDoubanService movieService = DoubanManager.createDoubanService();
         movieService.searchHotMovies().enqueue(callback);
+    }
+
+    //Movie's Adapter and view holder
+    static class MoviesAdapter extends RecyclerView.Adapter<MoviesViewHolder> {
+        private List<Movies> movies;
+        private Context context;
+
+        @LayoutRes
+        private int layoutResId;
+
+        public MoviesAdapter(Context context, @NonNull List<Movies> movies, @LayoutRes int layoutResId) {
+            this.movies = movies;
+            this.layoutResId = layoutResId;
+            this.context = context;
+        }
+
+        @Override
+        public MoviesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(context).inflate(layoutResId, parent, false);
+            return new MoviesViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MoviesViewHolder holder, int position) {
+            if (holder == null) return;
+
+            holder.updateMovie(movies.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.size();
+        }
+
+        public void setData(List<Movies> movies) {
+            this.movies = movies;
+            notifyDataSetChanged();
+        }
+    }
+
+    static class MoviesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView mMovieImage;
+        TextView mMovieTitle;
+        RatingBar mMovieStars;
+        TextView mMovieRatingAverage;
+        Movies movie;
+
+        public MoviesViewHolder(View itemView) {
+            super(itemView);
+
+            mMovieImage = (ImageView) itemView.findViewById(R.id.movie_cover);
+            mMovieTitle = (TextView) itemView.findViewById(R.id.movie_title);
+            mMovieStars = (RatingBar) itemView.findViewById(R.id.rating_star);
+            mMovieRatingAverage = (TextView) itemView.findViewById(R.id.movie_average);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void updateMovie(Movies movie) {
+            if (movie == null) return;
+            this.movie = movie;
+
+            Context context = itemView.getContext();
+
+            Picasso.with(context)
+                    .load(movie.getImages().getLarge())
+                    .placeholder(context.getResources().getDrawable(R.mipmap.ic_launcher))
+                    .into(mMovieImage);
+
+            mMovieTitle.setText(movie.getTitle());
+            final double average = movie.getRating().getAverage();
+            if (average == 0.0) {
+                mMovieStars.setVisibility(View.GONE);
+                mMovieRatingAverage.setText(context.getResources().getString(R.string.string_no_note));
+            } else {
+                mMovieStars.setVisibility(View.VISIBLE);
+                mMovieRatingAverage.setText(String.valueOf(average));
+                mMovieStars.setStepSize(0.5f);
+                mMovieStars.setRating((float) (movie.getRating().getAverage() / 2));
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.e(HomeActivity.TAG, "==> onClick....Item");
+
+            if (movie == null) return;
+            if (itemView == null) return;
+
+            Context context = itemView.getContext();
+            if (context == null) return;
+
+            Intent intent = new Intent(context, MovieDetailActivity.class);
+            intent.putExtra("movie", movie);
+
+            if (context instanceof Activity) {
+                Activity activity = (Activity) context;
+
+                Bundle bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, mMovieImage, "cover").toBundle();
+                ActivityCompat.startActivity(activity, intent, bundle);
+            }
+
+        }
     }
 
 }
